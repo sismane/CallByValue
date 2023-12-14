91285a4872c3aeb6077af9e82c37da963af20523
sismane <sismane@hotmail.com>
2023-12-14 15:28:59 +0300

    seiral id added

--- src/com/kagan/callbyvalue/exception/CustomerException.java
+++ src/com/kagan/callbyvalue/exception/CustomerException.java
@@ -2,6 +2,7 @@ package com.kagan.callbyvalue.exception;
 
 public class CustomerException extends Exception {
 
+	private static final long serialVersionUID = 1L;
 	private String explanation;
 
 	public CustomerException(String explanation) {
